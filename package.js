7d596af139d22b9e5289532b5e2a24c98a90bba4
Tarcísio Gruppi <txgruppi@gmail.com>
2012-12-27 15:29:24 -0200

    Removed some unnecessary code

--- package.js
+++ package.js
@@ -4,16 +4,11 @@ Package.describe({
   summary: "A markdown parser and compiler. Built for speed."
 });
 
-Package.on_use(function (api, where) {
-  where = where || ["client", "server"];
-  where = where instanceof Array ? where : [where];
+Package.on_use(function (api) {
+  // Common
+  api.add_files("marked/lib/marked.js", ["client", "server"]);
 
-  api.add_files("marked/lib/marked.js", where);
-
-  // XXX what we really want to do is, load template-integration after
-  // handlebars, iff handlebars was included in the project.
-  if (where === "client" || (where instanceof Array && where.indexOf("client") !== -1)) {
-    api.use("handlebars", "client");
-    api.add_files("template-integration.js", "client");
-  }
+  // Client only
+  api.use("handlebars", "client");
+  api.add_files("template-integration.js", "client");
 });
