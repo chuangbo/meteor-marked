5912ec7a5bc8f0394b7e673c3f68241bfd460ac6
Li Chuangbo <im@chuangbo.li>
2014-03-28 22:10:26 +1300

    support meteor 0.8.0. Thanks @gartz #10.

--- package.js
+++ package.js
@@ -4,19 +4,9 @@ Package.describe({
   summary: "A markdown parser and compiler. Built for speed."
 });
 
-var _ = Npm.require('underscore');
+Package.on_use(function (api) {
+  api.add_files("marked/lib/marked.js");
 
-Package.on_use(function (api, where) {
-  where = where || ["client", "server"];
-  where = where instanceof Array ? where : [where];
-
-  api.add_files("marked/lib/marked.js", where);
-
-  // XXX what we really want to do is, load template-integration after
-  // handlebars, iff handlebars was included in the project.
-  if (where === "client" ||
-      (where instanceof Array && _.indexOf(where, "client") !== -1)) {
-    api.use("handlebars", "client");
-    api.add_files("template-integration.js", "client");
-  }
+  api.use("ui", "client", {weak: true});
+  api.add_files('template-integration.js', 'client');
 });
