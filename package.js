847d08af6997b27f109f0fac046227dcbf9ef650
Li Chuangbo <im@chuangbo.li>
2014-08-27 15:25:17 +1200

    for meteor 0.9.0 official atmosphere

--- package.js
+++ package.js
@@ -1,10 +1,13 @@
 // Source: https://github.com/chjj/marked
 
 Package.describe({
-  summary: "A markdown parser and compiler. Built for speed."
+  summary: "A markdown parser and compiler. Built for speed.",
+  version: "0.3.3",
+  git: "https://github.com/chuangbo/meteor-marked.git"
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom("METEOR@0.9.0");
   api.add_files("marked/lib/marked.js");
 
   api.use("templating", "client", {weak: true});
