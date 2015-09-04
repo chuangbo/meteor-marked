1b4530311752c5627558bb09d8d37c42c8af1e0d
Gadi Cohen <dragon@wastelands.net>
2015-09-04 13:38:45 +0300

    v0.3.5_1; bump marked version (#20)

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 
 Package.describe({
   summary: "A markdown parser and compiler. Built for speed.",
-  version: "0.3.2_4",
+  version: "0.3.5_1",
   name: "chuangbo:marked",
   git: "https://github.com/chuangbo/meteor-marked.git"
 });
